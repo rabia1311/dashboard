76ece13d2c0137cf547fad759256607127eb99d1
rabia1311 <rabiasultana.n@gmail.com>
2023-08-20 21:53:09 +0530

    Bar chart included using google charts

---
+++ src/components/Charts/Barchat.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+const data = [
+    ["Year", "Sales", "Expenses", "Profit"],
+    ["2016", 1000, 400, 200],
+    ["2017", 1100, 600, 250],
+    ["2018", 900, 300, 300],
+    ["2019", 1200, 1000, 200],
+    ["2020", 800, 100, 400],
+  ];
+  const options = {
+    chart: {
+      title: "Company Performance",
+      subtitle: "Sales, Expenses, and Profit: 2016-2020",
+    },
+  };
+  const Barchat=()=>{
+    return (
+        <div>
+          <Chart
+            chartType="Bar"
+            data={data}
+            options={options}
+            width="100%"
+            height="500px"
+          />
+        </div>
+      );
+    };
+  export default Barchat;
--- src/components/Maindash/MainDash.js
+++ src/components/Maindash/MainDash.js
@@ -1,11 +1,14 @@
 import "../Maindash/maindash.css"
 import React from 'react'
 import Cards from "../Cards/Cards"
+import Barchat from "../Charts/Barchat"
 const MainDash = () => {
   return (
     <div className="MainDash">
         <h1>Dashboard</h1>
         <Cards/>
+        <hr></hr>
+        <Barchat/>
 
     </div>
   )
